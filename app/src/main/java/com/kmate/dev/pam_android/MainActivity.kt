77a6eed122384c9a16d50a808b8e40017fe099e5
kmate <kmate.dev@gmail.com>
2024-10-12 18:46:52 +0200

    Implemented simple ToDoScreen design

--- app/src/main/java/com/kmate/dev/pam_android/MainActivity.kt
+++ app/src/main/java/com/kmate/dev/pam_android/MainActivity.kt
@@ -3,13 +3,26 @@ package com.kmate.dev.pam_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kmate.dev.pam_android.domain.ToDoItem
 import com.kmate.dev.pam_android.ui.theme.PAM_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ToDoScreen(
+                        listOf(
+                            ToDoItem(
+                                "Prepare Android Example",
+                                false
+                            ),
+                            ToDoItem(
+                                "Prepare iOS Example",
+                                false
+                            )
+                        )
+                    )
                 }
             }
         }
@@ -30,17 +54,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ToDoScreen(todosList: List<ToDoItem>) {
+    Column {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = "",
+            onValueChange = {}
+        )
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onClick = {
+                //viewmodel....
+            }) {
+            Text(
+                text = "Add note"
+            )
+        }
+        LazyColumn(
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            items(todosList) {
+                ToDoItem(item = it)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ToDoItem(item: ToDoItem) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = item.name
+        )
+        RadioButton(
+            selected = item.completed,
+            onClick = {
+
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true, device = "id:pixel_5")
 @Composable
 fun GreetingPreview() {
     PAM_AndroidTheme {
-        Greeting("Android")
+        ToDoScreen(
+            listOf(
+                ToDoItem(
+                    "Prepare Android Example",
+                    false
+                ),
+                ToDoItem(
+                    "Prepare iOS Example",
+                    false
+                )
+            )
+        )
     }
 }
