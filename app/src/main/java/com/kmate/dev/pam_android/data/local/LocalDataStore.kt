96526c13baba11a85af8e2eaab296596210787a4
kmate <kmate.dev@gmail.com>
2024-11-10 17:58:26 +0100

    Implemented local data store

--- app/src/main/java/com/kmate/dev/pam_android/data/local/LocalDataStore.kt
+++ app/src/main/java/com/kmate/dev/pam_android/data/local/LocalDataStore.kt
@@ -1,9 +1,48 @@
 package com.kmate.dev.pam_android.data.local
 
+import android.content.SharedPreferences
 import com.kmate.dev.pam_android.domain.ToDoItem
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 interface LocalDataStore {
     fun updateToDoItems(items: List<ToDoItem>)
     fun getToDoItems(): List<ToDoItem>
     fun getToDoItem(itemId: Int): ToDoItem?
+}
+
+class LocalDataStoreImpl(private val sharedPreferences: SharedPreferences): LocalDataStore {
+    companion object {
+        private const val TO_DO_ITEMS_KEY = "to_do_items_key"
+    }
+
+    override fun getToDoItems(): List<ToDoItem> {
+        try {
+            val toDoItemsListString = sharedPreferences.getString(
+                TO_DO_ITEMS_KEY,
+                ""
+            )
+            val toDoItemsList = Json.decodeFromString<List<ToDoItem>>(toDoItemsListString!!)
+            return toDoItemsList
+        } catch (e: Exception) {
+            // Ignoring potential errors and returning empty list
+            return emptyList()
+        }
+    }
+
+    override fun updateToDoItems(items: List<ToDoItem>) {
+        try {
+            sharedPreferences.edit().putString(
+                TO_DO_ITEMS_KEY,
+                Json.encodeToString(items)
+            ).apply()
+        } catch (e: Exception) {
+            // Ignoring potential errors as this is just example app
+        }
+    }
+
+    override fun getToDoItem(itemId: Int): ToDoItem? {
+        val toDoItemsList = getToDoItems()
+        return toDoItemsList.find { it.id == itemId }
+    }
 }
--- app/src/main/java/com/kmate/dev/pam_android/di/AppModule.kt
+++ app/src/main/java/com/kmate/dev/pam_android/di/AppModule.kt
@@ -1,13 +1,22 @@
 package com.kmate.dev.pam_android.di
 
+import android.content.Context
+import com.kmate.dev.pam_android.data.local.LocalDataStore
+import com.kmate.dev.pam_android.data.local.LocalDataStoreImpl
 import com.kmate.dev.pam_android.ui.screens.item.ToDoItemViewModel
 import com.kmate.dev.pam_android.ui.screens.list.ToDoListViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single<String>{
-        "Hello Dependency Injection with Koin!"
+    single<LocalDataStore>{
+        LocalDataStoreImpl(
+            sharedPreferences = androidContext().getSharedPreferences(
+                "app_preferences",
+                Context.MODE_PRIVATE
+            )
+        )
     }
 
     viewModel {
