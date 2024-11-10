894db74d20ccc7bbd17aa2ba2b261c3411c8e4cf
kmate <kmate.dev@gmail.com>
2024-11-10 14:01:11 +0100

    Simple string injection example

--- app/src/main/java/com/kmate/dev/pam_android/ui/screens/item/ToDoItemScreen.kt
+++ app/src/main/java/com/kmate/dev/pam_android/ui/screens/item/ToDoItemScreen.kt
@@ -1,6 +1,7 @@
 package com.kmate.dev.pam_android.ui.screens.item
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,21 +14,28 @@ fun ToDoItemScreen(
     itemId: Int
 ) {
     ToDoItemScreen(
-        itemId = itemId
+        itemId = itemId,
+        injectedString = viewModel.injectedStringExample
     )
 }
 
 @Composable
 fun ToDoItemScreen(
-    itemId: Int
+    itemId: Int,
+    injectedString: String
 ) {
     Box(
         modifier = Modifier
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            "This screen should show details of item: $itemId"
-        )
+        Column {
+            Text(
+                injectedString
+            )
+            Text(
+                "This screen should show details of item: $itemId"
+            )
+        }
     }
 }
--- app/src/main/java/com/kmate/dev/pam_android/ui/screens/item/ToDoItemViewModel.kt
+++ app/src/main/java/com/kmate/dev/pam_android/ui/screens/item/ToDoItemViewModel.kt
@@ -1,7 +1,9 @@
 package com.kmate.dev.pam_android.ui.screens.item
 
 import androidx.lifecycle.ViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class ToDoItemViewModel: ViewModel() {
-    //Empty for now
+class ToDoItemViewModel: ViewModel(), KoinComponent {
+    val injectedStringExample: String by inject()
 }
